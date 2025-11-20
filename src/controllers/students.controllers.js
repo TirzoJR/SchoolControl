38d1e6bb14df056635e87557766b8d0f959352af
tirzomartinez2003@gmail.com <tirzomartinez2003@gmail.com>
2025-11-20 11:56:36 -0600

    Json

--- src/controllers/students.controllers.js
+++ src/controllers/students.controllers.js
@@ -5,8 +5,8 @@ const studentsControllers = {};
 studentsControllers.getAll = (req, res) => {
     studentsDaos.getAll()
         .then((students) => {
-            res.render("index.ejs",{
-                students
+            res.json({
+                data: students
             })
             
         })
@@ -22,7 +22,9 @@ studentsControllers.getOne = (req, res) => {
     studentsDaos.getOne(req.params.student_id)
         .then((student) => {
             if (student) {
-                res.render("edit.ejs",{student});
+                res.json({
+                    data: student
+                });
             } else {
                 res.status(404).json({
                     message: "Student not found"
@@ -40,7 +42,10 @@ studentsControllers.getOne = (req, res) => {
 studentsControllers.insertOne =async (req, res) => {
     studentsDaos.insertOne(req.body)
     .then((newStudent) => {
-        res.redirect("/api/students/getAll");
+        res.json({
+            message: "Student created successfully",
+            data: newStudent
+        });
     })
     .catch((err) => {
         res.status(500).json({
@@ -57,8 +62,10 @@ studentsControllers.updateOne = (req, res) => {
     studentsDaos.updateOne(req.params.student_id, req.body)
         .then((updatedStudent) => {
             if (updatedStudent) {
-                res.redirect("/api/students/getAll");
-            
+                res.json({
+                    message: "Student updated successfully",
+                    data: updatedStudent
+                });
         }else {
             res.status(404).json({
                 message: "Student not found"
@@ -79,7 +86,10 @@ studentsControllers.deleteOne = (req, res) => {
 studentsDaos.deleteOne(req.params.student_id)
     .then((deletedStudent) => {
         if (deletedStudent) {
-            res.redirect("/api/students/getAll");
+            res.json({
+                message: "Student deleted successfully",
+                data: deletedStudent
+            });
     }else{
         res.status(404).json({
             message: "Student not found"});
--- src/routes/students.routes.js
+++ src/routes/students.routes.js
@@ -5,8 +5,8 @@ const router = Router();
 router.get("/getAll",studentsControllers.getAll);
 router.get("/getOne/:student_id",studentsControllers.getOne);
 router.post("/insertOne",studentsControllers.insertOne);
-router.post("/updateOne/:student_id",studentsControllers.updateOne);
-router.get("/deleteOne/:student_id",studentsControllers.deleteOne);
+router.put("/updateOne/:student_id",studentsControllers.updateOne);
+router.delete("/deleteOne/:student_id",studentsControllers.deleteOne);
 
 
 
